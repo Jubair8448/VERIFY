7e9a25b2838aad6c22a5721e2bc24e572f9705d0
Jubair8448 <ansarijubair661@gmailcom>
2026-01-06 20:38:45 +0530

    fix route params for next 16

--- app/api/codes/delete/[id]/route.ts
+++ app/api/codes/delete/[id]/route.ts
@@ -1,23 +1,11 @@
-import { NextResponse } from "next/server";
-import { connectDB } from "@/lib/db";
-import Code from "@/lib/CodeModel";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { NextRequest, NextResponse } from "next/server";
 
-export async function DELETE(req: Request, { params }: { params: { id: string } }) {
-  try {
-    const { id } = params;
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
 
-    if (!id) return NextResponse.json({ msg: "❌ ID is required" }, { status: 400 });
-
-    await connectDB();
-
-    const code = await Code.findById(id);
-    if (!code) return NextResponse.json({ msg: "❌ Code not found" }, { status: 404 });
-
-    await code.deleteOne();
-
-    return NextResponse.json({ msg: "🗑 Code deleted successfully", success: true }, { status: 200 });
-  } catch (err) {
-    console.error("Delete Code Error:", err);
-    return NextResponse.json({ msg: "❌ Server error" }, { status: 500 });
-  }
+  return NextResponse.json({ msg: "Deleted" });
 }
